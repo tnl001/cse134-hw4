c4f695f3832679057095274e73bcd6ea3e0e4938
Tai Le <tnl001@ucsd.edu>
2023-02-26 03:06:17 -0800

    added crud: create method

---
+++ js/crud/blog.js
@@ -0,0 +1,32 @@
+import { createBlog, loadBlog } from './blogServices.js'
+
+window.addEventListener("load", init);
+
+function init() {
+    let addPrompt = document.getElementById("add-prompt");
+    let addForm = document.getElementById("add-form");
+    let addBtn = document.getElementById("add-btn");
+
+    let postTitle = document.getElementById("post-title");
+    let postDate = document.getElementById("post-date");
+    let postSummary = document.getElementById("post-sum");
+
+    loadBlog();
+
+    addBtn.addEventListener("click", () => {
+        addPrompt.showModal();
+        addForm.reset();
+    });
+
+    addPrompt.addEventListener("close", () => {
+        let blogData = {
+            "postTitle": postTitle.value,
+            "postDate": postDate.value,
+            "postSummary": postSummary.value
+        };
+
+        if (addPrompt.returnValue != "false") {
+            createBlog(blogData);
+        }
+    });
+}
